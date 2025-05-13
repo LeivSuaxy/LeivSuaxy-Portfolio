de9514fc8c2c485f43f5109a61cb0350f1c28e12
jleivsuaxy <jleivsuaxy@gmail.com>
2025-05-12 23:03:44 -0400

    Reset project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
-
-import preact from '@astrojs/preact';
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), preact()]
-});
+export default defineConfig({});
